b0019db451f9f0f90d475703e94a8c64a90c6182
Alina Slobodeniuk <akulalina25@gmail.com>
2022-12-18 16:39:14 +0200

    1заняття

---
+++ script.js/practice.js
@@ -0,0 +1,21 @@
+// const min =10;
+// const max = 20;
+// const num = +prompt('Прощу вас ввеси число')
+// // let result = 0;
+// // if (num < min){
+// // result = min;
+// // }else if(num > max){
+// // result = max;
+// // }else{
+// // result = num;
+// // }
+
+// let result = Math.max(min,Math.min(max, num))
+
+// alert(result);
+
+const str = 'string not starting with capital';
+
+const newStr = str[0].toUpperCase() + 
+
+console.log(newStr) не завершине
