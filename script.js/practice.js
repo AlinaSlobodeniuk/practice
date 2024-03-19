3e2f81927f37b1aa9802344a986eaf54f99af052
Alina Slobodeniuk <112395038+AlinaSlobodeniuk@users.noreply.github.com>
2024-03-19 02:46:22 +0200

    Update practice.js

--- script.js/practice.js
+++ script.js/practice.js
@@ -1,16 +1,52 @@
 // const min =10;
 // const max = 20;
 // const num = +prompt('Прощу вас ввеси число')
-// // let result = 0;
-// // if (num < min){
-// // result = min;
-// // }else if(num > max){
-// // result = max;
-// // }else{
-// // result = num;
-// // }
+// let result = 0;
+// if (num < min){
+// result = min;
+// }else if(num > max){
+// result = max;
+// }else{
+// result = num;
+// }
 
-// let result = Math.max(min,Math.min(max, num))
+// result = Math.max(min,Math.min(max, num))
 
 // alert(result);
+
+// Модуль 1
+
+// task-1.js
+
+// function makeTransaction (quantity, pricePerDroid) {
+//     let totalPrice = quantity * pricePerDroid;
+//     return `"You ordered ${quantity} droids worth ${totalPrice} credits!"`
+// };
+// console.log(makeTransaction(5, 3000));
+// console.log(makeTransaction(3, 1000));
+// console.log(makeTransaction(10, 500));
+
+// task-2.js
+
+// function getShippingMessage(country, price, deliveryFee) {
+//     let totalPrice = price + deliveryFee;
+//     return `"Shipping to ${country} will cost ${totalPrice} credits"`
+// };
+
+// console.log(getShippingMessage("Australia", 120, 50));
+// console.log(getShippingMessage("Germany", 80, 20));
+// console.log(getShippingMessage("Sweden", 100, 20));
+
+// task-3.js
+
+// function getElementWidth(content, padding, border) {
+//     let newContent = parseFloat(content);
+//     let newPadding = parseFloat(padding);
+//     let newBorder = parseFloat(border);
+//     return totalWidth = newContent + newPadding * 2 +newBorder * 2;
+// }
+
+// console.log(getElementWidth("50px", "8px", "4px")); // 74
+// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+// console.log(getElementWidth("200px", "0px", "0px")); // 200
 
