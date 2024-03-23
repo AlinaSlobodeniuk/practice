7557029bf4c121ef2bf835138463660c747daf86
Alina Slobodeniuk <112395038+AlinaSlobodeniuk@users.noreply.github.com>
2024-03-23 03:02:30 +0200

    модуль2

--- script.js/practice.js
+++ script.js/practice.js
@@ -50,3 +50,58 @@
 // console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 // console.log(getElementWidth("200px", "0px", "0px")); // 200
 
+// Модуль 2
+
+// function checkPassword(password) {
+//     const correctPassword = "jqueryismyjam";
+//     return password === correctPassword ? `Access granted` : `Access denied, wrong password!`;
+    
+//   }
+//   console.log(checkPassword("r3actsux"));
+//   console.log(checkPassword("angul4r1sl1f3"));
+//   console.log(checkPassword("jqueryismyjam"));
+
+
+// function isNumberInRange(start, end, number) {
+//     const newNumber = start <= number && end >= number ? true : false;
+
+//     console.log(newNumber)
+//   }
+
+// isNumberInRange(10, 30, 17);
+// isNumberInRange(10, 30, 5);
+// isNumberInRange(20, 50, 24);
+// isNumberInRange(20, 50, 76);
+
+
+// function getFileName(file) {
+// if ((file.indexOf(".") === -1)) {
+//     return file;
+// } else {
+//     return file.slice(0, file.indexOf('.'));
+// }
+
+//     return file.indexOf(".") === -1 ? file : file.slice(0, file.indexOf("."));
+//   }
+
+// console.log(getFileName("index"));
+// console.log(getFileName("app"));
+// console.log(getFileName("index.css"));
+// console.log(getFileName("index.js"));
+
+
+ 
+// function calculateTotal(number) {
+//    let totalNumber = 0;
+//     for (let i = 0; i <= number; i +=1){
+//       totalNumber += i;
+//     }
+//     return totalNumber;
+//   }
+  
+//   console.log(calculateTotal(18));
+//   console.log(calculateTotal(24));
+//   console.log(calculateTotal(3));
+
+
+
